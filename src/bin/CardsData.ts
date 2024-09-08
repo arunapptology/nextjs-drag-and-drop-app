d08629b603b015718b3e0b8ba35df49b7b1a8273
arunapptology <arun.outlook1992@gmail.com>
2024-09-08 13:56:59 +0530

    my first drag and drom repo

---
+++ src/bin/CardsData.ts
@@ -0,0 +1,60 @@
+export const cardsData = [
+  {
+    id: 0,
+    title: "Section 1",
+    components: [
+      {
+        id: 100,
+        name: "Item 1"
+      },
+      {
+        id: 200,
+        name: "Item 2"
+      },
+    ]
+  },
+  {
+    id: 1,
+    title: "Section 2",
+    components: [
+      {
+        id: 300,
+        name: "Item 3"
+      },
+      {
+        id: 400,
+        name: "Item 4"
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "Section 3",
+    components: [
+      {
+        id: 500,
+        name: "Item 5"
+      },
+      {
+        id: 600,
+        name: "Item 6"
+      },
+      {
+        id: 700,
+        name: "Item 7"
+      },
+      {
+        id: 800,
+        name: "Item 8"
+      },
+      {
+        id: 900,
+        name: "Item 9"
+      },
+    ]
+  }
+
+
+]
+
+
